e8ce24781d9d69cedafb360788f0c957f140faba
Cameron Miller <62472008+Cameron-Miller-Git@users.noreply.github.com>
2022-12-26 17:37:30 -0700

    Add files via upload

---
+++ Sort/adder.py
@@ -0,0 +1,45 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "699cf086-65a4-4b13-ac75-47067e534e5b",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "name = \"AskPython\"\n",
+    "def add(num1, num2):\n",
+    "        return num1 + num2"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "47338cb6-d274-417c-b31c-6f713834ed60",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
